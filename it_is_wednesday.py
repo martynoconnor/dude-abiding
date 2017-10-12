f486844cf752a9ddf32c4c13822b7c4772a9e84a
Martyn O'Connor <contact@martynoconnor.net>
2017-10-12 17:53:10 +0100

    Updated the text for more duderish pronunciation.

--- it_is_wednesday.py
+++ it_is_wednesday.py
@@ -8,10 +8,9 @@ today_num = today.weekday()
 
 platform = platform.platform()
 if "Darwin" in platform:
-
     while today_num == 2:
-        os.system("say it is wednesday my dudes!")
+        os.system("say it is wednesday my dewds!")
     else:
-	    os.system("say My dudes, it is not Wednesday.")
+	    os.system("say My dewds, it is not Wednesday.")
 else:
-    print("Buy a Mac, my dudes.")
+    print("Buy a Mac, my dewds.")
